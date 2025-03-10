2e54a7d15cb7d906be30492334a8f5f29c44cfb9
GamingHazard <bb466882@gmail.com>
2025-03-10 18:23:30 +0300

    first commit

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/register-admin", async (req, res) => {
   try {
     const { names, contact, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email, contact });
     if (existingUser) {
       return res.status(400).json({ message: "Email already registered" });
     }
